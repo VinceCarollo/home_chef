6476e54825b3cc5e48a2161f1646ce1e8bcd2a35
vincecarollo <vincecarollo@gmail.com>
2020-03-30 14:09:33 -0600

    Change to use .sass extension

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import "../stylesheets/main.scss";
+import "../stylesheets/main.sass";
 
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
