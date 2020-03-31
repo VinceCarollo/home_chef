94ad93b39b067b2cba42926d36071c4194dd8cdb
vincecarollo <vincecarollo@gmail.com>
2020-03-31 11:17:51 -0600

    Add chefs table

---
+++ db/migrate/20200331164525_create_chefs.rb
@@ -0,0 +1,19 @@
+class CreateChefs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :chefs do |t|
+      t.string :email, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :city, null: false
+      t.string :state, null: false
+      t.string :zip, null: false
+      t.integer :age
+      t.integer :years_of_pro_experience
+      t.integer :years_of_am_experience
+      t.text :self_description, null: false
+      t.text :unavailable
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,34 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_03_31_164525) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "chefs", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "city", null: false
+    t.string "state", null: false
+    t.string "zip", null: false
+    t.integer "age"
+    t.integer "years_of_pro_experience"
+    t.integer "years_of_am_experience"
+    t.text "self_description", null: false
+    t.text "unavailable"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
