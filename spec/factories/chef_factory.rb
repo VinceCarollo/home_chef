50325693d12ec16c45624908d93846b24f6f509f
vincecarollo <vincecarollo@gmail.com>
2020-03-31 16:23:23 -0600

    Remove attributes from chef factory

--- spec/factories/chef_factory.rb
+++ spec/factories/chef_factory.rb
@@ -3,9 +3,6 @@ FactoryBot.define do
     first_name { "John" }
     last_name  { "Doe" }
     email  { "JohnDoe@example.com" }
-    city { "Kansas City" }
-    state { "MO" }
-    zip { '64086' }
     self_description { 'Best chef ever' }
   end
 end
