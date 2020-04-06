985e8acc8b39de001aeba1cbc17b5dfcaf27c6cf
vincecarollo <vincecarollo@gmail.com>
2020-04-06 10:58:50 -0600

    Move stylesheets to assets folder

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import "../stylesheets/main.sass";
+import "../../assets/stylesheets/main.sass";
 
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
