b9ce7f7c6a523d872d68efffa3e75dadc6bda455
vincecarollo <vincecarollo@gmail.com>
2020-04-09 15:01:38 -0600

    Remove color styling from js function

--- app/javascript/alerts/index.js
+++ app/javascript/alerts/index.js
@@ -1,7 +1,6 @@
 export function showWarning(msg) {
   let alert = document.getElementById('alert')
   alert.style.display = 'inline-block'
-  alert.style.backgroundColor = '#9e1c1c'
   alert.children[0].innerHTML = 'Passwords do not match'
 }
 
