91c867a376bc546e7db055fecd9bcd8bfcae8773
vincecarollo <vincecarollo@gmail.com>
2020-04-17 12:45:13 -0600

    Modify alert js function to account for objects

--- app/javascript/alerts/index.js
+++ app/javascript/alerts/index.js
@@ -1,7 +1,19 @@
 export function showWarning(msg) {
   let alert = document.getElementById('alert')
   alert.style.display = 'inline-block'
-  alert.children[0].innerHTML = 'Passwords do not match'
+
+  if(typeof msg === 'object') {
+    let messages = ''
+    for (let attribute in msg) {
+      let attributeString = attribute.replace('_', ' ')
+      messages = messages + attributeString.charAt(0).toUpperCase() + attributeString.slice(1) + ' '
+      messages = messages + msg[attribute] + '<br /> <br />'
+    }
+    
+    alert.children[0].innerHTML = messages
+  } else {
+    alert.children[0].innerHTML = msg
+  }
 }
 
 export function removeWarning() {
