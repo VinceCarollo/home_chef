e304980ff747091c4c137974fdff2e273d2ba745
vincecarollo <vincecarollo@gmail.com>
2020-04-17 12:50:21 -0600

    Start new chef create actions

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -2,4 +2,41 @@ class ChefsController < ApplicationController
   def new
     @chef = Chef.new
   end
+
+  def create
+    @chef = Chef::CreateService.perform(chef_params)
+    respond_to do |format|
+      if @chef.errors.present?
+        format.json { render json: @chef.errors, status: :unprocessable_entity }
+      else
+        format.json { head :ok }
+      end
+    end
+  end
+
+  private
+
+  def chef_params
+    params.require(:chef).permit(
+      :email,
+      :password,
+      :password_confirmation,
+      :first_name,
+      :last_name,
+      :age,
+      :zip,
+      :travel_distance,
+      :years_of_pro_experience,
+      :years_of_am_experience,
+      :self_description,
+      :specialty,
+      :monday,
+      :tuesday,
+      :wednesday,
+      :thursday,
+      :friday,
+      :saturday,
+      :sunday
+    )
+  end
 end
--- app/javascript/chef/new_chef.js
+++ app/javascript/chef/new_chef.js
@@ -1,6 +1,49 @@
+import { showWarning, removeWarning } from '../alerts/index'
+
 const newChef = () => {
-  console.log('hi')
-  console.log(event);
+  let password = document.getElementById("chef_password")
+  let passwordConf = document.getElementById("chef_password_confirmation")
+
+  passwordConf.addEventListener('keyup', () => {
+    if(password.value != passwordConf.value) {
+      showWarning('Paswords do not match')
+    } else {
+      removeWarning();
+    }
+  })
+
+  password.addEventListener('keyup', () => {
+    if (passwordConf.value != '') {
+      if (password.value != passwordConf.value) {
+        showWarning('Paswords do not match')
+      } else {
+        removeWarning();
+      }
+    }
+  })
+
+
+  $(document).ready(() => {
+    console.log('hi');
+
+    $('#new-chef-form')
+      .on("ajax:error", (e) => {
+        let status = e.originalEvent.detail[2].status
+        let response = e.originalEvent.detail[2].response
+        
+        if(status === 422) { 
+          showWarning(JSON.parse(response));
+        } else {
+          showWarning('An error occurred, please try again');
+        }
+      })
+      .on("ajax:success", (event, data, status, xhr) => {
+        console.log(event);
+        console.log(data);
+        console.log(status);
+        console.log(xhr);
+      });
+  });
 }
 
 export default newChef
