06119e95589f68773e0a181f50f8a517deaa97f1
vincecarollo <vincecarollo@gmail.com>
2020-04-18 21:59:01 -0600

    Add landing page redirect

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    
+    @chef = Chef.find_by(id: session[:chef_id])
+    if @chef
+      redirect_to '/chefs/dashboard'
+    end
   end
 end
