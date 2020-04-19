366cf95e87a9dd7255bc805c60d889601fac01d7
vincecarollo <vincecarollo@gmail.com>
2020-04-18 22:26:13 -0600

    Change js for new chef form to display only first error

--- app/javascript/alerts/index.js
+++ app/javascript/alerts/index.js
@@ -2,12 +2,17 @@ export function showWarning(msg) {
   let alert = document.getElementById('alert')
   alert.style.display = 'inline-block'
 
-  if(typeof msg === 'object') {
+  if (typeof msg === 'object') {
     let messages = ''
     for (let attribute in msg) {
       let attributeString = attribute.replace('_', ' ')
+      let error = msg[attribute]
+
       messages = messages + attributeString.charAt(0).toUpperCase() + attributeString.slice(1) + ' '
-      messages = messages + msg[attribute] + '<br /> <br />'
+      
+      if (error.length > 1) { error = error[0] }
+
+      messages = messages + error + '<br /> <br />'
     }
     
     alert.children[0].innerHTML = messages
