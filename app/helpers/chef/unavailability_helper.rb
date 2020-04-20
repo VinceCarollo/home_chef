f531da3f0a8fe2d0f850d38939b81c65b17713c2
vincecarollo <vincecarollo@gmail.com>
2020-04-20 12:27:20 -0600

    Remove pry

--- app/helpers/chef/unavailability_helper.rb
+++ app/helpers/chef/unavailability_helper.rb
@@ -3,7 +3,6 @@ module Chef::UnavailabilityHelper
     # Creates strings from form checkboxes 
     # chef.unavailable = 'mon, tue, wed'
     # input = {"monday"=>"0", "tuesday"=>"1", ...} | 0 = unavailable
-    binding.pry
     Date::DAYNAMES.map(&:downcase).map do |day|
       day[0..2] if weekday_choices[day.to_sym] == '0'
     end.compact.join(', ')
