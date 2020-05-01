838f70a4969400dbc6ed33fa72a5f7fea2d0f303
vincecarollo <vincecarollo@gmail.com>
2020-05-01 13:43:07 -0600

    Add login box animation with user choices

--- app/javascript/packs/login.js
+++ app/javascript/packs/login.js
@@ -1,37 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
-  let link = document.getElementById('login-box');
+  const link = document.getElementById('login-box');
 
   link.addEventListener('click', () => {
     link.style.width = '320px';
     link.style.height = '107px';
     link.style.backgroundColor = 'white';
-    link.style.fontSize = '50px'
-    link.style.color = 'black'
+    link.style.fontSize = '50px';
+    link.style.color = 'black';
     
-    let loginEl = document.getElementById('login-box')
     let chefButton = document.createElement('BUTTON');
     let clientButton = document.createElement('BUTTON');
+    
+    chefButton.setAttribute('class', 'login-choices');
+    clientButton.setAttribute('class', 'login-choices');
+    
+    chefButton.innerHTML = 'Chef';
+    clientButton.innerHTML = 'Client';
+    
+    link.addEventListener('webkitTransitionEnd', () => {
+      link.append(chefButton);
+      link.append(clientButton);
+    })
 
-    chefButton.setAttribute('class', 'login-choices')
-    clientButton.setAttribute('class', 'login-choices')
-
-    chefButton.innerHTML = 'Chef'
-    clientButton.innerHTML = 'Client'
-
-    loginEl.append(chefButton)
-    loginEl.append(clientButton)
-
+    chefButton.addEventListener('click', () => {
+      startChefEmailPrompt();
+    })
   }, { once: true })
 
   const startChefEmailPrompt = () => {
+    link.style.height = '220px';
+
     let emailBox = document.createElement('INPUT');
     let emailBoxLbl = document.createElement('P');
 
-    emailBoxLbl.innerHTML = 'Email'
+    emailBoxLbl.innerHTML = 'Email';
     emailBoxLbl.setAttribute("id", "login-email-lbl");
     emailBox.setAttribute("type", "text");
     emailBox.setAttribute("id", "email-box");
-    loginEl.append(emailBoxLbl)
-    loginEl.append(emailBox)
+
+    link.appendChild(emailBoxLbl);
+    link.appendChild(emailBox);
   }
 });
