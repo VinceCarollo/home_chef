de98ee709c4c0cf4528e084be5b0fc39c7aa8e93
vincecarollo <vincecarollo@gmail.com>
2020-05-06 15:39:28 -0600

    Add chef base controller spec

---
+++ spec/controllers/chef/base_controlle_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Chef::BaseController, type: :controller do
+  controller(Chef::BaseController) do
+    def index
+      head :ok
+    end
+  end
+
+  describe 'before actions' do
+    describe 'authorize' do
+      let!(:chef) { FactoryBot.create(:chef) }
+
+      it 'authorizes correct credentials' do
+        session[:chef_id] = chef.id
+        get :index
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'boots out the unwanted' do
+        session[:chef_id] = chef.id + 1
+        get :index
+        expect(response).to redirect_to('/')
+        expect(session[:chef_id]).to be nil
+      end
+    end
+  end
+end
