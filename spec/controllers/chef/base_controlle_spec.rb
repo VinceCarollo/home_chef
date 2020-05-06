8fc6588b5035f396f2110290f07a127b725423ed
vincecarollo <vincecarollo@gmail.com>
2020-05-06 15:46:15 -0600

    Refactor chef base controller spec

--- spec/controllers/chef/base_controlle_spec.rb
+++ spec/controllers/chef/base_controlle_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe Chef::BaseController, type: :controller do
   describe 'before actions' do
     describe 'authorize' do
       let!(:chef) { FactoryBot.create(:chef) }
+      let(:bad_chef_id) { chef.id + 1 }
 
       it 'authorizes correct credentials' do
         session[:chef_id] = chef.id
@@ -18,7 +19,7 @@ RSpec.describe Chef::BaseController, type: :controller do
       end
 
       it 'boots out the unwanted' do
-        session[:chef_id] = chef.id + 1
+        session[:chef_id] = bad_chef_id
         get :index
         expect(response).to redirect_to('/')
         expect(session[:chef_id]).to be nil
