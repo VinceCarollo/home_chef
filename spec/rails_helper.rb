1bb902892c98acc942ef52e279553b5c8b64ba28
vincecarollo <vincecarollo@gmail.com>
2020-05-14 21:36:11 -0600

    Add geocodio api key to VCR config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -75,4 +75,5 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
+  config.filter_sensitive_data('<GEOCODIO_API_KEY>') { ENV['GEOCODIO_API_KEY'] }
 end
