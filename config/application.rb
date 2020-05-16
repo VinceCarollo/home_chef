7489107bfb07f1f104614c437a7d9d7711df175d
vincecarollo <vincecarollo@gmail.com>
2020-05-15 22:19:41 -0600

    Add action mailer host url for url links in emails

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module HomeChef
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.action_mailer.default_url_options = { host: ENV['HOST_URL'] }
   end
 end
