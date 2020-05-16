7201e432707432f3e1c89a188892e9a885918e9f
vincecarollo <vincecarollo@gmail.com>
2020-05-16 13:47:35 -0600

    Add toastr wrapper function

---
+++ app/javascript/messages/index.js
@@ -0,0 +1,20 @@
+import toastr from 'toastr'
+
+export default function showMsg(msg, type) {
+  if (typeof msg === 'object') {
+    // For resource errors
+    Object.entries(msg).forEach((error) => {
+      let combinedError = error[0] + ' ' + error[1][0]
+      let formattedError = combinedError.replace('_', ' ')
+      toastr.warning(formattedError)
+    })
+  } else {
+    if (type === 'warning') {
+      toastr.warning(msg);
+    } else if (type === 'error') {
+      toastr.error(msg);
+    } else if (type === 'success') {
+      toastr.success(msg);
+    }
+  }
+}
