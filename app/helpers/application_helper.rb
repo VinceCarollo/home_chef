9e8311044d2cc4730679f9342fba6ad375878f00
vincecarollo <vincecarollo@gmail.com>
2020-05-16 16:18:50 -0600

    Render flash messages with js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
   def toastr_flash
+    # binding.pry
     flash.each_with_object([]) do |(type, message), flash_messages|
       type = 'success' if type == 'notice'
       type = 'error' if type == 'alert'
-      text = "<script>toastr.#{type}('#{message}', '', { closeButton: true, progressBar: true })</script>"
+      text = "<script>toastr.#{type}('#{message}')</script>"
       flash_messages << text.html_safe if message
     end.join("\n").html_safe
   end
