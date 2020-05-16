dc55a899d4be834892d35e0610e1915115961bd2
vincecarollo <vincecarollo@gmail.com>
2020-05-15 22:24:29 -0600

    Add chef confirm email route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
 
-  resources :chefs, only: [:new, :create, :update]
+  resources :chefs, only: [:new, :create, :update] do
+    member do
+      get :confirm_email
+    end
+  end
 
   delete 'chefs/logout', to: 'chef/sessions#destroy'
   post 'chefs/login', to: 'chef/sessions#create'
