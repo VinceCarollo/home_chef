06d7f7adfbd3a27383640e13c8124da51980e6d7
vincecarollo <vincecarollo@gmail.com>
2020-05-16 16:28:35 -0600

    Change flash notice to show as toastr warning instead of error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     # binding.pry
     flash.each_with_object([]) do |(type, message), flash_messages|
       type = 'success' if type == 'notice'
-      type = 'error' if type == 'alert'
+      type = 'warning' if type == 'alert'
       text = "<script>toastr.#{type}('#{message}')</script>"
       flash_messages << text.html_safe if message
     end.join("\n").html_safe
