adbeb4f862c8629a4ef8dfaab9d3118a7f6271ac
vincecarollo <vincecarollo@gmail.com>
2020-05-16 18:11:30 -0600

    Refactor toastr_flash method for readability

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
   def toastr_flash
     # binding.pry
     flash.each_with_object([]) do |(type, message), flash_messages|
-      type = 'success' if type == 'notice'
-      type = 'warning' if type == 'alert'
-      text = "<script>toastr.#{type}('#{message}')</script>"
+      toastr_type = 'success' if type == 'notice'
+      toastr_type = 'warning' if type == 'alert'
+      text = "<script>toastr.#{toastr_type}('#{message}')</script>"
       flash_messages << text.html_safe if message
     end.join("\n").html_safe
   end
