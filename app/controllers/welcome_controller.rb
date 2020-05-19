3769111f33e11b2a29f699b6eae4643a797055d9
vincecarollo <vincecarollo@gmail.com>
2020-05-19 15:57:56 -0600

    Add check for session before db call

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   skip_before_action :authorize, only: :index
 
   def index
+    return unless session[:chef_id]
     @chef = Chef.find_by(id: session[:chef_id])
     if @chef
       redirect_to chefs_dashboard_path
