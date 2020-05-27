8a60b42ed9c5a4e12477af83b199873f82a9e00e
vincecarollo <vincecarollo@gmail.com>
2020-05-26 22:19:08 -0600

    Add password matching warnings to new client form

--- app/javascript/client/new_client.js
+++ app/javascript/client/new_client.js
@@ -1,8 +1,20 @@
 import showMsg from '../messages/index'
 
 const newChef = () => {
-  console.log('new chef');
+  let password = document.getElementById('password')
+  let passwordConf = document.getElementById('password_confirmation')
   
+  passwordConf.addEventListener('focusout', () => {
+    if (password.value != passwordConf.value) {
+      showMsg('Paswords Do Not Match', 'warning')
+    }
+  })
+
+  password.addEventListener('focusout', () => {
+    if (passwordConf.value != '' && password.value != passwordConf.value) {
+      showMsg('Passwords Do Not Match', 'warning')
+    }
+  })
 }
 
 export default newChef
