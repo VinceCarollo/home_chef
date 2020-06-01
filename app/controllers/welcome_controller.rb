aafdade7c6d1e004d32ab8336ccd4a2c82a46d5a
vincecarollo <vincecarollo@gmail.com>
2020-06-01 16:33:20 -0600

    Change homepage to redirect to client dashboard if session

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,14 @@ class WelcomeController < ApplicationController
   skip_before_action :authorize, only: :index
 
   def index
-    return unless session[:chef_id]
+    return unless session[:chef_id] || session[:client_id]
 
-    @chef = Chef.find_by(id: session[:chef_id])
-    redirect_to chefs_dashboard_path if @chef
+    @chef = Chef.find_by_id(session[:chef_id])
+    @client = Client.find_by_id(session[:client_id])
+    if @chef
+      redirect_to chefs_dashboard_path
+    elsif @client
+      redirect_to clients_dashboard_path
+    end
   end
 end
