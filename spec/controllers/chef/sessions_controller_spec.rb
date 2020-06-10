374e8bd5588c221e9219d703df1be7d290fde81f
vincecarollo <vincecarollo@gmail.com>
2020-06-09 21:36:07 -0600

    Add destroy spec to both sessions controllers

--- spec/controllers/chef/sessions_controller_spec.rb
+++ spec/controllers/chef/sessions_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Chef::SessionsController, type: :controller do
-  describe 'logging in' do
+  describe 'create' do
     let!(:chef) do
       FactoryBot.create(
         :chef,
@@ -38,6 +38,22 @@ RSpec.describe Chef::SessionsController, type: :controller do
 
       expect(session[:chef_id]).to be nil
       expect(flash[:alert]).to eq('Invalid Login Credentials')
+    end
+  end
+  
+  describe 'destroy' do
+    before do
+      session[:chef_id] = 1234
+    end
+
+    it 'destroys session' do
+      delete :destroy
+      expect(session[:chef_id]).to be nil
+    end
+
+    it 'redirects home' do
+      delete :destroy
+      expect(response).to redirect_to(:root)
     end
   end
 end
