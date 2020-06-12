c0fca92d209a810f25f4735d631bbcedc16834cc
vincecarollo <vincecarollo@gmail.com>
2020-06-11 21:26:01 -0600

    Start local chefs page

--- app/javascript/users/index.js
+++ app/javascript/users/index.js
@@ -1,7 +1,8 @@
 import newClient from './client/new_client';
 import newChef from './chef/new_chef';
-import chefDashboard from './chef/dashboard'
-import clientDashboard from './client/dashboard'
+import chefDashboard from './chef/dashboard';
+import chefIndex from './chef/chef_index';
+import clientDashboard from './client/dashboard';
 
 document.addEventListener('DOMContentLoaded', () => {
   if (Object.keys(document.getElementsByClassName("new-client-form-container")).length) {
@@ -12,5 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
     chefDashboard();
   } else if (document.getElementById('client-dashboard')) {
     clientDashboard();
+  } else if (document.getElementById('chefs-show-page')) {
+    chefIndex();
   }
 });
